8e623ef92d6e91e694bcde877d49ae93efbf9322
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2020-08-25 10:04:25 +1000

    MDL-68792 theme_boost: Do not move blocks on embedded layout.

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1838,7 +1838,9 @@ class core_renderer extends renderer_base {
                 }
                 $lastblock = $bc->title;
             } else if ($bc instanceof block_move_target) {
-                $output .= $this->block_move_target($bc, $zones, $lastblock, $region);
+                if (!$fakeblocksonly) {
+                    $output .= $this->block_move_target($bc, $zones, $lastblock, $region);
+                }
             } else {
                 throw new coding_exception('Unexpected type of thing (' . get_class($bc) . ') found in list of block contents.');
             }
