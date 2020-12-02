9635e3cc20b504f7384e15bba6589d1449794d42
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2020-12-02 19:34:29 +1100

    MDL-70323 core_h5p: Add H5P crossOrigin setting

--- admin/settings/h5p.php
+++ admin/settings/h5p.php
@@ -41,4 +41,7 @@ if (!empty($defaulth5plib)) {
 
     $settings->add(new admin_settings_h5plib_handler_select('h5plibraryhandler', new lang_string('h5plibraryhandler', 'core_h5p'),
         new lang_string('h5plibraryhandler_help', 'core_h5p'), $defaulth5plib));
+
+    $settings->add(new admin_setting_configselect('h5pcrossorigin', new lang_string('h5pcrossorigin', 'core_h5p'),
+        new lang_string('h5pcrossorigin_help', 'core_h5p'), 0, [0 => '', 1 => 'anonymous']));
 }
